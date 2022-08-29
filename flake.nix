78da707037ea673094ad0348d567b9872ea3b1d2
SirStoke <sandro.mosca.dev@gmail.com>
2022-08-29 09:07:27 +0200

    Include a flake.nix file

---
+++ flake.nix
@@ -0,0 +1,32 @@
+# Essentially copy-pasted from nix-community/naersk
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+
+          config.allowUnfree = true;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+        
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        # For `nix develop` (optional, can be skipped):
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo jetbrains.idea-ultimate ];
+        };
+      }
+    );
+}
